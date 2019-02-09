5da76bb0821a1d1f91eb236da11260805e2323d2
Eric Shelley <eric@webdesignbakery.com>
2019-02-09 16:23:25 -0700

    Adding setup script to gh-pages

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded #
+
+set -e
+set -o pipefail
+
+echo -e "\033[36m~> \033[34mInstalling github-actions-slack...\033[39m"
+curl -o /usr/local/bin/github-actions-slack http://bartimae.us/github-actions-slack/setup.sh
+chmod +x /usr/local/bin/github-actions-slack
+
+echo ""
+echo -e "\033[92mgithub-actions-slack is ready to go!"
+
+} # this ensures the entire script is downloaded #
