d1f46640578f35943daae7ddfe2e0d2c396519a5
Eric Shelley <eric@webdesignbakery.com>
2019-02-09 16:34:04 -0700

    Removing jq dependency from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@
 set -e
 set -o pipefail
 
-echo -e "\033[36m~> \033[34mInstalling dependencies...\033[39m"
-which jq || {
-  apt update && apt install -y jq
-}
-
 echo -e "\033[36m~> \033[34mInstalling github-actions-slack...\033[39m"
 curl -o /usr/local/bin/github-actions-slack http://bartimae.us/github-actions-slack/setup.sh
 chmod +x /usr/local/bin/github-actions-slack
